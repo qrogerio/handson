0096ddbef04295f86ca5b637a80a6964f35772c0
Aluno <aluno@edu.senac.br>
2023-09-06 20:50:31 -0300

    handsonn

---
+++ js/handson06.js
@@ -0,0 +1,11 @@
+function impaoupar(){
+    var numero=parseInt(document.getElementById("numero").value)
+    if(numero % 2 ==0 ){
+        alert("par")}
+        else{
+            alert("impa")
+        }
+    
+    }
+
+ 
